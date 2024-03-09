b0dacaa3bb7a3242c52075b7bec008a378e078a5
Hudaibkhan <161836104+Hudaibkhan@users.noreply.github.com>
2024-03-10 01:47:18 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,123 @@
+// Day-8-challenge
+// QUESTION 22 
+// let array: (string | number) []=['hudaib','absar',1,2,3,'aliyan']
+// console.log(array [5])
+
+
+
+
+
+// // QUESTION 23
+// let car: string= 'subaru';
+
+// // Test 1: Equality comparison (True)
+// console.log("Is car == 'subaru'? I predict True.");
+// console.log(car == 'subaru');
+
+// // Test 2: Strict Equality comparison (True)
+// console.log("Is car === 'subaru'? I predict True.");
+// console.log(car === 'subaru');
+
+// // Test 3: Inequality Equality comparison (False)
+// console.log("Is car != 'subaru'? I predict false.");
+// console.log(car != 'subaru');
+
+// // Test 4: Strict Inequality Equality comparison (False)
+// console.log("Is car !== 'subaru'? I predict false.");
+// console.log(car !== 'subaru');
+
+// // Test 5: Less than comparison (False)
+// console.log("Is car < 'subaru'? I predict false.");
+// console.log(car < 'subaru');
+
+// // Test 6: Greater than comparison (False)
+// console.log("Is car > 'subaru'? I predict false.");
+// console.log(car > 'subaru');
+
+// // Test 7: Less than or equal comparison (True)
+// console.log("Is car <= 'subaru'? I predict True.");
+// console.log(car <= 'subaru');
+
+// // Test 8: Greater than or equal comparison (True)
+// console.log("Is car >= 'subaru'? I predict True.");
+// console.log(car >= 'subaru')
+
+// // Test 9: Checking truthiness (True)
+// console.log("Is car? I predict True.");
+// console.log(car);
+
+// // Test 9: Checking falsiness (False)
+// console.log("Is !car? I predict false.");
+// console.log(!car);
+
+
+
+
+
+
+
+// QUESTION 24
+// Equality with strings
+// console.log("Testing equality with strings:");
+// console.log("apple"=="apple");  //True
+// console.log("apple"=="Apple");  //False
+
+//Using the lower case function
+// console.log("Testing with lower case:");
+// console.log("Apple".toLowerCase()=="apple"); //True
+
+//Numerical tests
+// console.log("Numerical tests:");
+// console.log(20 > 10); //True
+// console.log(4 < 2); //False
+
+//Tests Using 'and' and 'or' operators
+// console.log("Tests with 'and' and 'or':");
+// console.log(true && false); //False
+// console.log(true false); //True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
